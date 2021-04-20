a828bfbe012271ac6bf4abe4661c4319b1d0a3f7
Queendy38 <42394077+Queendy38@users.noreply.github.com>
2021-04-20 03:34:11 -1000

    Update script.js

--- script.js
+++ script.js
@@ -29,6 +29,9 @@
             strength.push(mydata.applicants[i].attributes[i].strength);
             spicy.push(mydata.applicants[i].attributes[i].spicyFoodTolerance);
         } 
+        
+        //display data from JSON
+        //document.getElementById('demo2').innerHTML = IQ[1] + en[1] + strength[1] + spicy[1];
         document.getElementById('demo').innerHTML = IQ[0] + " " + en[0] + " " + strength[0] + " " + spicy[0];
 
 
@@ -53,6 +56,3 @@
 function confirm(){
     document.getElementById('confirmMsg').innerHTML = "Added " + applicantName.value + " as new applicant";
 }
-
- //display data from JSON
-           //document.getElementById('demo2').innerHTML = IQ[1] + en[1] + strength[1] + spicy[1];
