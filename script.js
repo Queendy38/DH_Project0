1170f0e78d1b5c59f3f414ef576d690130f2bac8
Queendy38 <42394077+Queendy38@users.noreply.github.com>
2021-04-20 12:13:36 -1000

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
   var url = "input.json";
   var scoredApp = [];
   var score = [];
-  var appObj=[];
+  var appObj = [];
   var mydata;
   //check status
   xmlhttp.onreadystatechange = function () {
@@ -15,89 +15,87 @@
           var currentTeamName = []; //array to store member in team
 
           //attributes for all applicants
-          var it=[]; 
-          var en=[];
-          var strength=[];
-          var spicy=[];
+          var it = [];
+          var en = [];
+          var strength = [];
+          var spicy = [];
 
           //load content from JSON to array
           for (var i = 0; i < mydata.team.length; i++) {
               currentTeamName.push(mydata.team[i].name);
-          } 
-         
+          }
+
           for (var i = 0; i < mydata.applicants.length; i++) {
               //mydata.team[0].attributes[0].endurance
-            currentAppName.push(mydata.applicants[i].name);
-            it.push(mydata.applicants[i].attributes[0].intelligence); //scale to 20% 
-            en.push(mydata.applicants[i].attributes[0].endurance); //scale to 40%
-            strength.push(mydata.applicants[i].attributes[0].strength); //scale to 30%
-            spicy.push(mydata.applicants[i].attributes[0].spicyFoodTolerance); //scale to 10%
+              currentAppName.push(mydata.applicants[i].name);
+              it.push(mydata.applicants[i].attributes[0].intelligence); //scale to 20% 
+              en.push(mydata.applicants[i].attributes[0].endurance); //scale to 40%
+              strength.push(mydata.applicants[i].attributes[0].strength); //scale to 30%
+              spicy.push(mydata.applicants[i].attributes[0].spicyFoodTolerance); //scale to 10%
 
-            //calculate score
-            score.push(calculate(it[i],strength[i],en[i],spicy[i]));
-        } 
-        // mydata.applicants.push({name: "yolo", attributes: {strength:strength[0]}});
-        //display data from JSON
-        //document.getElementById('demo').innerHTML = JSON.stringify(mydata);
-        document.getElementById('demo2').innerHTML = score;
+              //calculate score
+              score.push(calculate(it[i], strength[i], en[i], spicy[i]));
+          }
+          // mydata.applicants.push({name: "yolo", attributes: {strength:strength[0]}});
+          //display data from JSON
+          //document.getElementById('demo').innerHTML = JSON.stringify(mydata);
+          document.getElementById('demo2').innerHTML = score;
 
-       // document.getElementById('demo').innerHTML = it[0] + " " + en[0] + " " + strength[0] + " " + spicy[0];
+          // document.getElementById('demo').innerHTML = it[0] + " " + en[0] + " " + strength[0] + " " + spicy[0];
+          //display team members
+          function displayTeam() {
+              document.getElementById('team'), innerHTML = JSON.stringify(mydata.team, null, 10);
+          }
 
+          function displayApp() {
+              document.getElementById('team'), innerHTML = JSON.stringify(mydata.team, null, 10);
 
+          }
+          console.log("current team members");
+          console.log(JSON.stringify(mydata, null, 10));
       }
   };
-  
+
   xmlhttp.open("GET", url, true);
   xmlhttp.send();
 
 
-/*function displayValue() {
-            var ele = document.getElementsByName('score4');
-              
-            for(i = 0; i < ele.length; i++) {
-                if(ele[i].checked)
-                document.getElementById("demo").innerHTML
-                        = "Selected: " +ele[i].value;
-            }
-        }
-*/
+  /*function displayValue() {
+              var ele = document.getElementsByName('score4');
+                
+              for(i = 0; i < ele.length; i++) {
+                  if(ele[i].checked)
+                  document.getElementById("demo").innerHTML
+                          = "Selected: " +ele[i].value;
+              }
+          }
+  */
 
-//display a confirm message after adding new applicant
-//score values in json arrays
-function confirm(){
-    //document.getElementById('confirmMsg').innerHTML = "Added " + applicantName.value + " as new applicant";
-    var ele = document.getElementsByName('score4');
-              
-    for(i = 0; i < ele.length; i++) {
-        if(ele[i].checked)
-        document.getElementById("demo").innerHTML
-                = "Selected: " +ele[i].value;
-    }
+  //display a confirm message after adding new applicant
+  //score values in json arrays
+  function confirm() {
+      //document.getElementById('confirmMsg').innerHTML = "Added " + applicantName.value + " as new applicant";
+      var ele = document.getElementsByName('score4');
 
-    document.getElementById('confirmMsg').innerHTML = "Added " + applicantName.value + " as new applicant";
-   
-}
-//display team members
-function displayTeam(){
-    document.getElementById('team'),innerHTML = JSON.stringify(mydata.team, null, 10);
-}
-function displayApp(){
-    document.getElementById('team'),innerHTML = JSON.stringify(mydata.team,null, 10);
+      for (i = 0; i < ele.length; i++) {
+          if (ele[i].checked)
+              document.getElementById("demo").innerHTML = "Selected: " + ele[i].value;
+      }
 
-}
-//calculate score
-function calculate(it, str, en, spicy){
-  var score, finalScore;
+      document.getElementById('confirmMsg').innerHTML = "Added " + applicantName.value + " as new applicant";
 
-  //apply scales
-  score = (it * 0.2) + (str * 0.3) + (en * 0.4) + (spicy * 0.1);
-  //convert to 0-1 scale
-  finalScore = score * (0.2);
+  }
 
-  return finalScore.toFixed(2);
-}
-//read iuput from html + exisiting input, show in console
-//output json in console
-    console.log("current team members");
-    console.log(JSON.stringify(mydata, null, 10));
-        
+  //calculate score
+  function calculate(it, str, en, spicy) {
+      var score, finalScore;
+
+      //apply scales
+      score = (it * 0.2) + (str * 0.3) + (en * 0.4) + (spicy * 0.1);
+      //convert to 0-1 scale
+      finalScore = score * (0.2);
+
+      return finalScore.toFixed(2);
+  }
+  //read iuput from html + exisiting input, show in console
+  //output json in console
