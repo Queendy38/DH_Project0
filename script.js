9137fc2a8462335828181929961a8ee3d4dc692a
Queendy38 <42394077+Queendy38@users.noreply.github.com>
2021-04-20 01:17:25 -1000

    added form

---
+++ script.js
@@ -0,0 +1,27 @@
+  //create form to get input from users
+  //create xmlhttprequest
+  var xmlhttp = new XMLHttpRequest();
+  var url = "input.json";
+
+  //check status
+  xmlhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+          var mydata = JSON.parse(this.responseText);
+
+          var nameApp = []; //array to store applicants
+          var nameTeam = []; //array to store member in team
+
+          //load content from JSON to array
+          for (var i = 0; i < mydata.team.length; i++) {
+              nameApp.push(mydata.team[i].name);
+
+          }
+
+          document.getElementById('').innerHTML = JSON.stringify(mydata);
+
+      
+      }
+  };
+  
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
